190ef342136f96a69ea3772c954d62b33969c8bb
Yoojin Lee <yoojinLee.dev@gmail.com>
2025-02-12 21:20:17 +0900

    feat: 상품 엔티티 추가

---
+++ src/main/java/com/gangnam/wholesale/domain/product/Product.java
@@ -0,0 +1,33 @@
+package com.gangnam.wholesale.domain.product;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "products")
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private String name;
+
+    private String description;
+
+    private String imageUrl;
+
+    //상품 매입가
+    private BigDecimal wholesaleCost;
+
+    //상품 기본 판매가
+    private BigDecimal basePrice;
+
+}
